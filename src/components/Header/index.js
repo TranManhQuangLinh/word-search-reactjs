94f68844b71463fcbeeeaa276f0b601f6fe8ce7b
Tran Manh Quang Linh <L3032001@gmail.com>
2023-02-24 10:40:29 +0700

    fix change tag bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,30 +24,39 @@ function Header() {
             <nav className="navbar navbar-expand-lg d-flex flex-column" style={{ backgroundColor: "white !important" }}>
                 <img src={logo} className="rounded mx-auto d-block" style={{ height: "100px", width: '200px' }} alt="" />
                 <div className="container-fluid">
-                    
-                        <ul className='navbar-nav'>
-                            <li className='nav-item'>
-                                <Link id="spell_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong am' })} aria-current="page" to="/DongAm" onClick={() => {
-                                    if(word){dispatch(setInfoText(''))}
-                                    dispatch(setType('dong am'))
-                                    dispatch(setSearchCheck(!searchCheck))
-                                }}>Đồng âm</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link id="mean_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong nghia' })} to="/DongNghia" onClick={() => {
-                                    if(word){dispatch(setInfoText(''))}
-                                    dispatch(setType('dong nghia'))
-                                    dispatch(setSearchCheck(!searchCheck))
-                                }}>Đồng nghĩa</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link id='antonyms' className={clsx('nav-link', styles.tag, { 'active': type === 'trai nghia' })} to="/TraiNghia" onClick={() => {
-                                    if(word){dispatch(setInfoText(''))}
-                                    dispatch(setType('trai nghia'))
-                                    dispatch(setSearchCheck(!searchCheck))
-                                }}>Trái nghĩa</Link>
-                            </li>
-                        </ul>
+
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                            <Link id="spell_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong am' })} aria-current="page" to="/DongAm" onClick={() => {
+                                if (word) {
+                                    dispatch(setInfoText(''))
+                                    dispatch(setResultList([]))
+                                }
+                                dispatch(setType('dong am'))
+                                dispatch(setSearchCheck(!searchCheck))
+                            }}>Đồng âm</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link id="mean_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong nghia' })} to="/DongNghia" onClick={() => {
+                                if (word) {
+                                    dispatch(setInfoText(''))
+                                    dispatch(setResultList([]))
+                                }
+                                dispatch(setType('dong nghia'))
+                                dispatch(setSearchCheck(!searchCheck))
+                            }}>Đồng nghĩa</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link id='antonyms' className={clsx('nav-link', styles.tag, { 'active': type === 'trai nghia' })} to="/TraiNghia" onClick={() => {
+                                if (word) {
+                                    dispatch(setInfoText(''))
+                                    dispatch(setResultList([]))
+                                }
+                                dispatch(setType('trai nghia'))
+                                dispatch(setSearchCheck(!searchCheck))
+                            }}>Trái nghĩa</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
