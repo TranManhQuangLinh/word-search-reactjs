347ef23033a9ebcdaeafbd4f19964f294dff0076
Tran Manh Quang Linh <L3032001@gmail.com>
2023-02-24 13:47:43 +0700

    responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,42 +21,47 @@ function Header() {
 
     return (
         <header className={clsx("container-fluid", styles.header)}>
-            <nav className="navbar navbar-expand-lg d-flex flex-column" style={{ backgroundColor: "white !important" }}>
+            <nav className="navbar navbar-dark navbar-expand-lg d-flex flex-column" style={{ backgroundColor: "white !important" }}>
                 <img src={logo} className="rounded mx-auto d-block" style={{ height: "100px", width: '200px' }} alt="" />
-                <div className="container-fluid">
-
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <Link id="spell_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong am' })} aria-current="page" to="/DongAm" onClick={() => {
-                                if (word) {
-                                    dispatch(setInfoText(''))
-                                    dispatch(setResultList([]))
-                                }
-                                dispatch(setType('dong am'))
-                                dispatch(setSearchCheck(!searchCheck))
-                            }}>Đồng âm</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link id="mean_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong nghia' })} to="/DongNghia" onClick={() => {
-                                if (word) {
-                                    dispatch(setInfoText(''))
-                                    dispatch(setResultList([]))
-                                }
-                                dispatch(setType('dong nghia'))
-                                dispatch(setSearchCheck(!searchCheck))
-                            }}>Đồng nghĩa</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link id='antonyms' className={clsx('nav-link', styles.tag, { 'active': type === 'trai nghia' })} to="/TraiNghia" onClick={() => {
-                                if (word) {
-                                    dispatch(setInfoText(''))
-                                    dispatch(setResultList([]))
-                                }
-                                dispatch(setType('trai nghia'))
-                                dispatch(setSearchCheck(!searchCheck))
-                            }}>Trái nghĩa</Link>
-                        </li>
-                    </ul>
+                <div className="container-fluid justify-content-end">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className='navbar-nav'>
+                            <li className='nav-item'>
+                                <Link id="spell_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong am' })} aria-current="page" to="/DongAm" onClick={() => {
+                                    if (word) {
+                                        dispatch(setInfoText(''))
+                                        dispatch(setResultList([]))
+                                    }
+                                    dispatch(setType('dong am'))
+                                    dispatch(setSearchCheck(!searchCheck))
+                                }}>Đồng âm</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link id="mean_like" className={clsx('nav-link', styles.tag, { 'active': type === 'dong nghia' })} to="/DongNghia" onClick={() => {
+                                    if (word) {
+                                        dispatch(setInfoText(''))
+                                        dispatch(setResultList([]))
+                                    }
+                                    dispatch(setType('dong nghia'))
+                                    dispatch(setSearchCheck(!searchCheck))
+                                }}>Đồng nghĩa</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link id='antonyms' className={clsx('nav-link', styles.tag, { 'active': type === 'trai nghia' })} to="/TraiNghia" onClick={() => {
+                                    if (word) {
+                                        dispatch(setInfoText(''))
+                                        dispatch(setResultList([]))
+                                    }
+                                    dispatch(setType('trai nghia'))
+                                    dispatch(setSearchCheck(!searchCheck))
+                                }}>Trái nghĩa</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
